10daccdf4e7aa6bf9a26798dc997c38503fb1f4a
KehaoHuang <huangk53@mcmaster.ca>
2022-10-25 20:16:38 -0400

    Formatting Users page

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,7 +3,14 @@ import React from 'react';
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-    const USERS = [{id: 'u1', name: 'Max Paro', image: 'https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg', places: 8}];
+    const USERS = [
+        {
+            id: 'u1', 
+            name: 'Max Paro', 
+            image: 'https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg', 
+            places: 8
+        }
+    ];
 
     return <UsersList items={USERS}/>
 };
