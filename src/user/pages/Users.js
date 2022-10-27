fd4b5e00064e38875859c3e516fd9ee7380af341
KehaoHuang <huangk53@mcmaster.ca>
2022-10-26 22:13:03 -0400

    Format code

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import UsersList from '../components/UsersList';
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-    const USERS = [
-        {
-            id: 'u1', 
-            name: 'Max Paro', 
-            image: 'https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg', 
-            places: 8
-        }
-    ];
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Paro",
+      image:
+        "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+      places: 8,
+    },
+  ];
 
-    return <UsersList items={USERS}/>
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
